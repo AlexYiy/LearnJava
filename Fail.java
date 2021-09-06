93e7cd2dda91a7aef7274a526c550c204cfda898
Alex Yi <67743450+AlexYiy@users.noreply.github.com>
2021-09-06 02:50:43 +0000

    Add files via upload

---
+++ Fail.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+import java.util.HashMap;
+public class Main {
+    public static void main(String[] args) {
+        //Scanner in = new Scanner(System.in);
+        //int a = in.nextInt();
+        //System.out.println(a);
+        System.out.println("Hello World!");
+    }
+    
+}
+
+class MyDate {
+    public int year;
+    public int month;
+    public int day;
+    HashMap<Integer, Integer> monthDay = new HashMap<Integer, Integer>();
+    monthDay.put(1, 31);
+    monthDay.put(3, 31);
+    monthDay.put(5, 31);
+    monthDay.put(7, 31);
+    monthDay.put(8, 31);
+    monthDay.put(10, 31);
+    monthDay.put(12, 31);
+    public MyDate(int year, int month, int day) throws Exception {
+        if (year < 1 || year > 9999) {
+            throw new IllegalArgumentException("year error");
+        }
+        if (month < 1 || month > 12) {
+            throw new IllegalArgumentException("month error");
+        }
+        if (day < 1 || day > 31) {
+            throw new IllegalArgumentException("day error");
+        }
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+    public static int sub (MyDate d1, MyDate d2) {
+        int daysFromMonth;
+        if (d1.month == 2) {
+            if (d1.year%4) {
+                daysFromMonth = 28
+            } else {
+                
+            }
+        }
+         = monthDay.getOrDefault(d1.month, 30);
+        d1.year * 365 + d1.month 
+    }
+}
+
